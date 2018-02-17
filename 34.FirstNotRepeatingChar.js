8f82c5b6e43a9d8b46d344046d99920929808176
luowendi <luowendi@umier.com.cn>
2018-02-17 21:55:27 +0800

    add FirstNotRepeatingChar

---
+++ 34.FirstNotRepeatingChar.js
@@ -0,0 +1,9 @@
+function FirstNotRepeatingChar(str){
+    // write code here
+    for(var i=0;i<str.length;i++){
+        if(str.indexOf(str[i]) === str.lastIndexOf(str[i])){
+            return i
+        }
+    }
+    return -1
+}
