201cb47cf62c59ad68cf6d3e6d768d1a5f2f22e4
luowendi <luowendi@umier.com.cn>
2018-02-23 21:30:57 +0800

    add Add

---
+++ 48.Add.js
@@ -0,0 +1,11 @@
+function Add(num1, num2){
+    // write code here
+    var sum,carry;
+    do {
+        sum = num1 ^ num2
+        carry = (num1 & num2) << 1;
+        num1 = sum
+        num2 = carry
+    }while(num2 !== 0);
+    return num1
+}
