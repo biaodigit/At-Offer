c0a4bcf310a08ccb703c04334b4207a0a2573610
luowendi <15622285015@163.com>
2018-08-16 19:13:39 +0800

    rewrite power

--- 12.power.js
+++ 12.power.js
@@ -1,3 +1,24 @@
-function Power(base, exponent) {
-    return Math.pow(base, exponent)
+function Power(x, n) {
+    // write code here
+    if (n < 0) {
+        if (x <= 0) {
+            throw new Error("分母不能小于等于0");
+        } else {
+            if (-n % 2 === 1) {
+                return 1 / (Power(x, -n - 1) * x)
+            } else {
+                var r = 1 / (Power(x, -n / 2))
+                return r * r
+            }
+        }
+    } else if (n === 0) {
+        return 1
+    } else {
+        if (n % 2 === 1) {
+            return Power(x, n - 1) * x
+        } else {
+            var r = Power(x, n / 2)
+            return r * r
+        }
+    }
 }
