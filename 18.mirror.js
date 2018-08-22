9fa508acd4a0e0690f4808b438f5acc3d5802234
luowendi <15622285015@163.com>
2018-08-22 22:10:56 +0800

    update

--- 18.mirror.js
+++ 18.mirror.js
@@ -1,17 +1,17 @@
-function Mirror(root){
+function Mirror(root) {
     // write code here
-    if(!root){
-        return null
-    }
-    let stack = []
-    stack.push(root)
-    while(stack.length){
-        let node = stack.pop()
-        if(node.left) stack.push(node.left)
-        if(node.right) stack.push(node.right)
-        let temp = node.left
-        node.left = node.right
-        node.right = temp
+    if (!root) return null
+    let stack = [],
+        node,
+        temp;
+    stack.push(root);
+    while (stack.length) {
+        node = stack.pop();
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+        temp = node.left;
+        node.left = node.right;
+        node.right = temp;
 
     }
     return root
